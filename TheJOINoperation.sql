b968c7d301a25833496d9cf2da1bb47e57a1bf1b
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-07-04 11:22:46 -0400

    Update TheJOINoperation.sql

--- TheJOINoperation.sql
+++ TheJOINoperation.sql
@@ -43,5 +43,13 @@ JOIN goal
 ON game.id = goal.matchid
 WHERE player LIKE 'Mario%';
 
+/*5. The table eteam gives details of every national team including the coach. You can JOIN goal to eteam using the phrase goal JOIN eteam on teamid=id
 
+Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10 */
+
+SELECT player, teamid, coach, gtime
+  FROM goal 
+JOIN eteam
+ON goal.teamid = eteam.id
+ WHERE gtime<=10;
 
