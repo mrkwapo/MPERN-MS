a892ad9060ed6cd96c7bbbd61142b77f1989c8ae
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2020-07-04 11:13:37 -0400

    Update TheJOINoperation.sql

--- TheJOINoperation.sql
+++ TheJOINoperation.sql
@@ -33,3 +33,15 @@ SELECT goal.player, goal.teamid, game.stadium, game.mdate
 JOIN goal 
 ON (game.id=goal.matchid)
 WHERE teamid = 'GER';
+
+/*4. Use the same JOIN as in the previous question.
+
+Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'*/
+SELECT game.team1, game.team2, goal.player
+FROM game
+JOIN goal
+ON game.id = goal.matchid
+WHERE player LIKE 'Mario%';
+
+
+
